4f5bb9178e9268189442b482a07670898c1ac57e
Yevhen Smoliy <52038205+SmoliyY@users.noreply.github.com>
2023-08-21 15:30:44 +0300

    Update Counter.tsx

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export function Counter() {
-  const [count, setCount] = React.useState(2);
+  const [count, setCount] = React.useState(3);
 
   return (
     <div style={{ border: '1px solid red', padding: '10px' }}>
